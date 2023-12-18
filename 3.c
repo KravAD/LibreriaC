c71f8735b3d8ade53ac8cdf0f56d6d2d0e131394
KravAD <dkrav@myuax.com>
2023-12-18 13:31:25 +0100

    commit 30
    Declaro un nuevo puntero de tipo FILE i lo condiciono con que si no lo lo puede abrir salta mensaje de que  no se pudo abrir para escribir.

--- 3.c
+++ 3.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 
+
 int main () {
 
     FILE *file = fopen("file.txt", "r");
-    if (file = NULL){
-        printf("El archivo no se abre. \n");
-        return 1;
-    }
 
-    fclose(file);
+        if (file == NULL) {
+            printf("El archivo no se abre.\n");
+            return 1;
+        }
+
+        FILE *file2 = fopen("file.txt","w");
+        if (file == NULL){
+            printf("El archivo no se pudo abrir para escribir.\n");
+            return 1;
+        }
+
+        fclose(file);
 
     return 0;
 }
