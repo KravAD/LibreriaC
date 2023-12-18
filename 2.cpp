456c9b5781f1eac42ec25cb19f0760d09aa55708
KravAD <dkrav@myuax.com>
2023-12-18 12:54:49 +0100

    commit 17
    Creo una tercera cadena para poder calcular su longitud.

--- 2.cpp
+++ 2.cpp
@@ -6,6 +6,8 @@ int main() {
     char string1[] = "Moto";
     char string2[] = "Coche";
 
+    char string3[] = "La vida es como una lenteja, o la tomas o la dejas";
+
     int comparacion = strcmp(string1, string2);
 
     if (comparacion == 0 ) {
