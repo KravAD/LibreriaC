7b7c6d43ccfc4fba19e1814d64255b39513bbb0f
KravAD <dkrav@myuax.com>
2023-12-18 13:15:10 +0100

    commit 27
    Modifico la condición.

--- 3.c
+++ 3.c
@@ -4,7 +4,8 @@ int main () {
 
     FILE *file = fopen("file.txt", "r");
     if (file = NULL){
-        printf("El archivo no existe");
+        printf("El archivo no se abre. \n");
+        return 1;
     }
 
     return 0;
